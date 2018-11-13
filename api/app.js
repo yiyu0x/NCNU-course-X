2e3d12721e9895207226d3a6e85e8fa515831543
yiyu0x <tako0903@icloud.com>
2018-11-14 00:36:57 +0800

    api_server_run

--- api/app.js
+++ api/app.js
@@ -1,8 +1,32 @@
-const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://localhost/ncnu', {useNewUrlParser: true});
-const db = mongoose.connection;
-const Schema = new mongoose.Schema({});
-const Mongo = mongoose.model('Mongo', Schema, 'class');
-Mongo.find({}, function (err, data) {
-	console.log(data)
+const {Mongo, fields} = require('./db_config');
+const express = require('express');
+const app = express();
+
+//CORS fixed
+app.all('*', function (req, res, next) {
+    // console.log('debug: ', req.method)
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-type');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'PUT,POST,GET,DELETE,OPTIONS,PATCH'
+    );
+    res.header('Access-Control-Max-Age', 1728000);
+    //post CORS
+    next();
+});
+
+app.get('/api/', function (req, res) {
+	Mongo.find({}, fields, {limit: 100, sort: {cousre_id: 1}}, function (err, doc) {
+		if (err) res.status(500).send('API error');
+		else res.send(doc);
+	});
+});
+
+// Mongo.find({}, fields, {sort: {cousre_id: 1}}, function (err, data) {
+// 	console.log(data)
+// });
+
+app.listen(5487, () => {
+    console.log(':web => http://127.0.0.1:5487/api');
 });
