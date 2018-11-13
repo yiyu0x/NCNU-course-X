11a754f523d07327963032a391df7a86ead259c8
yiyu0x <tako0903@icloud.com>
2018-11-14 01:55:37 +0800

    frontend data filter

---
+++ src/components/TableView.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-card dark>
+        <v-card-title>
+            課程資訊
+            <v-spacer></v-spacer>
+            <v-text-field v-model="search" append-icon="search" label="萬用篩選器 (任意輸入文字來篩選結果)" single-line hide-details></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="info" :loading="isLoading" :search="search" hide-actions class="elevation-1" dark>
+            <v-progress-linear slot="progress" color="green" indeterminate></v-progress-linear>
+            <template slot="items" slot-scope="props">
+                <td class="text-xs">{{ props.item.course_cname }}</td>
+                <td class="text-xs">{{ props.item.cousre_id }}</td>
+                <td class="text-xs">{{ props.item.department }}</td>
+                <td class="text-xs">{{ props.item.division }}</td>
+                <td class="text-xs">{{ props.item.course_credit }}</td>
+                <td class="text-xs">{{ props.item.time }}</td>
+                <td class="text-xs">{{ props.item.location }}</td>
+                <td class="text-xs">{{ props.item.teacher }}</td>
+            </template>
+        </v-data-table>
+    </v-card>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            search: '',
+            headers: [{ text: '課程名稱', align: 'left', value: 'course_cname', sortable: false },
+                { text: '課號', value: 'cousre_id' },
+                { text: '開課單位', value: 'department' },
+                { text: '年級', value: 'division' },
+                { text: '學分', value: 'course_credit' },
+                { text: '時段', value: 'time' },
+                { text: '地點', value: 'location' },
+                { text: '教師姓名', value: 'teacher' }
+
+            ],
+            info: [],
+            isLoading: true
+        }
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:5487/api')
+            .then(response => {
+                this.info = response.data;
+                this.isLoading = false;
+            });
+    }
+}
+</script>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card color="grey lighten-4" flat height="200px" tile>
+        <v-toolbar prominent extended dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>暨大課程查詢系統</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn>問題回報</v-btn>
+                <v-btn>參與協助</v-btn>
+            </v-toolbar-items>
+            <v-btn icon>
+                <v-icon>search</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>favorite</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>more_vert</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </v-card>
+</template>
