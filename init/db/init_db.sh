3fd0ea8fd084fbbda2a45bef2ea162667bacfb70
yiyu0x <tako0903@icloud.com>
2018-11-14 00:12:01 +0800

    api_server

--- init/db/init_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/docker/volumes/db-data/
-node ../data/app.js
-cp ../data/course_data.json ~/docker/volumes/db-data/
-# docker run -d -v ~/docker/volumes/db-data/:/tmp mongo
-docker run -p 27017:27017 -v ~/docker/volumes/db-data:/tmp -d mongo
-mongoimport  --db ncnu --collection class --file tmp/course_data.json --jsonArray
