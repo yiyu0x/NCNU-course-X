d483d59b9fb9d4191ba05235d70df9d8fb2b9bd3
yiyu0x <tako0903@icloud.com>
2018-11-14 16:46:28 +0800

    web_server CORS fixed

--- web_server/app.js
+++ web_server/app.js
@@ -3,6 +3,22 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
+
+
+//CORS fixed
+app.all('*', function (req, res, next) {
+    // console.log('debug: ', req.method)
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-type');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'PUT,POST,GET,DELETE,OPTIONS,PATCH'
+    );
+    res.header('Access-Control-Max-Age', 1728000);
+    //post CORS
+    next();
+});
+
 app.get('/', function (req, res) {
     var html = fs.readFileSync(
         path.resolve(__dirname, '../dist/index.html'),
