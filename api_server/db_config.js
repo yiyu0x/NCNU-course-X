9d5e3a3518025ad1c4ea9fc1b68e2479a7513b85
yiyu0x <tako0903@icloud.com>
2018-11-14 15:47:47 +0800

    error catch in db_config

--- api_server/db_config.js
+++ api_server/db_config.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://localhost/ncnu', {useNewUrlParser: true});
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ncnu', { useNewUrlParser: true }, function(err, db) {
+    if (err) {
+        console.log('Unable to connect to the server. Please start the server. Error:', err);
+    } else {
+        console.log('Connected to Server successfully!');
+    }
+});
+
+
+
+
 const db = mongoose.connection;
 const Schema = new mongoose.Schema({});
 const Mongo = mongoose.model('Mongo', Schema, 'class');
@@ -8,19 +18,19 @@ const Mongo = mongoose.model('Mongo', Schema, 'class');
 //});
 
 const fields = {
-	_id: 0,
-	course_cname: 1,
-	course_credit: 1,
-	cousre_id: 1,
-	department: 1,
-	division: 1,
-	faculty: 1,
-	location: 1,
-	teacher: 1,
-	time: 1
+    _id: 0,
+    course_cname: 1,
+    course_credit: 1,
+    cousre_id: 1,
+    department: 1,
+    division: 1,
+    faculty: 1,
+    location: 1,
+    teacher: 1,
+    time: 1
 };
 
 module.exports = {
-	Mongo,
-	fields
+    Mongo,
+    fields
 }
