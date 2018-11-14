d885a217d9e246fcdf052997361d655f3ea10eae
yiyu0x <tako0903@icloud.com>
2018-11-14 13:31:54 +0800

    web server

---
+++ web_server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+app.get('/', function (req, res) {
+    var html = fs.readFileSync(
+        path.resolve(__dirname, '../dist/index.html'),
+        'utf-8'
+    );
+    res.send(html);
+});
+
+app.listen(8066, () => {
+    console.log(':web => http://127.0.0.1:8066');
+});
