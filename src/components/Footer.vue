c0eba983042b5bd1240870588c532eadb4a46e0b
yiyu0x <tako0903@icloud.com>
2018-11-14 21:59:54 +0800

    dev

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-footer dark height="auto">
+        <v-card class="flex" flat tile>
+            <v-card-actions class="grey darken-3 justify-center">
+                &copy;2018 - <strong>yiyu0x</strong>
+            </v-card-actions>
+        </v-card>
+    </v-footer>
+</template>
