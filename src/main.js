df63f57f8fb44be647703b168022f70978732d8d
yiyu0x <tako0903@icloud.com>
2018-11-18 11:43:30 +0800

    api server update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 export const serverBus = new Vue();
-export const base_url = 'http://163.22.17.89:5488';
+export const base_url = 'https://api.ncnusa.ml';
 Vue.config.productionTip = false
 
 new Vue({
