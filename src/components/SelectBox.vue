8845a337f3ecd46371aa9e10e6f8af302a0e330b
aFish <tako0903@icloud.com>
2019-01-15 01:13:55 +0800

    1072 link to ncnu web system

--- src/components/SelectBox.vue
+++ src/components/SelectBox.vue
@@ -1,117 +1,147 @@
 <template>
-    <v-container fluid grid-list-xl>
-        <v-layout wrap align-center>
-            <v-flex xs12 sm6 d-flex>
-                <v-select v-model="fac" @change="getDataFac(fac)" :items="faculty" label="學院"></v-select>
-            </v-flex>
-            <v-flex xs12 sm6 d-flex>
-                <v-select v-model="dep" @change="getDataDep(dep)" :items="department" label="系所"></v-select>
-            </v-flex>
-        </v-layout>
-        <v-layout row wrap>
-            <v-flex xs12 sm6 md3>
-                <v-autocomplete :loading="loading_teacher" :items="items_teacher" :search-input.sync="search_teacher" v-model="teacher_name" cache-items class="mx-3" flat hide-no-data hide-details label="教師姓名" solo-inverted @input="getDataTeacher(teacher_name)"></v-autocomplete>
-            </v-flex>
-            <v-flex xs12 sm6 md3>
-                <v-autocomplete :loading="loading_course" :items="items_course" :search-input.sync="search_course" v-model="cname" cache-items class="mx-3" flat hide-no-data hide-details label="課程名稱" solo-inverted @input="getDataCourse(cname)"></v-autocomplete>
-                <!--                 <v-text-field v-model="cname" @input="getDataCourse(cname)" label="課程名稱"></v-text-field> -->
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid grid-list-xl>
+    <v-layout wrap align-center>
+      <v-flex xs12 sm6 d-flex>
+        <v-select v-model="fac" @change="getDataFac(fac)" :items="faculty" label="學院"></v-select>
+      </v-flex>
+      <v-flex xs12 sm6 d-flex>
+        <v-select v-model="dep" @change="getDataDep(dep)" :items="department" label="系所"></v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md3>
+        <v-autocomplete
+          :loading="loading_teacher"
+          :items="items_teacher"
+          :search-input.sync="search_teacher"
+          v-model="teacher_name"
+          cache-items
+          class="mx-3"
+          flat
+          hide-no-data
+          hide-details
+          label="教師姓名"
+          solo-inverted
+          @input="getDataTeacher(teacher_name)"
+        ></v-autocomplete>
+      </v-flex>
+      <v-flex xs12 sm6 md3>
+        <v-text-field
+          flat
+          hide-no-data
+          hide-details
+          v-model="cname"
+          label="課名模糊搜尋"
+          solo-inverted
+          @input="getDataCourse(cname)"
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 md3>
+        <v-autocomplete
+          :loading="loading_course"
+          :items="items_course"
+          :search-input.sync="search_course"
+          v-model="cname"
+          cache-items
+          class="mx-3"
+          flat
+          hide-no-data
+          hide-details
+          label="課名精確搜尋"
+          solo-inverted
+          @input="getDataCourse(cname)"
+        ></v-autocomplete>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
-import axios from 'axios';
-import { serverBus, base_url } from '../main';
+import axios from "axios";
+import { serverBus, base_url } from "../main";
 
 export default {
-    data: () => ({
-        faculty: ['人文學院', '管理學院', '科技學院', '教育學院'],
-        department: [],
-        fac: '',
-        dep: '',
-        teacher_name: '',
-        cname: '',
-        items_teacher: [],
-        items_course: [],
-        loading_teacher: false,
-        loading_course: false,
-        search_teacher: null,
-        search_course: null,
-        select_teacher: null,
-        select_course: null,
-        teacher_name_box: [],
-        course_name_box: []
-    }),
-    mounted() {
-        axios.get(base_url + '/api/teacherList/all')
-            .then(response => {
-                this.teacher_name_box = response.data;
-            });
-        axios.get(base_url + '/api/courseList/all')
-            .then(response => {
-                this.course_name_box = response.data;
-            });
-
+  data: () => ({
+    faculty: ["人文學院", "管理學院", "科技學院", "教育學院"],
+    department: [],
+    fac: "",
+    dep: "",
+    teacher_name: "",
+    cname: "",
+    items_teacher: [],
+    items_course: [],
+    loading_teacher: false,
+    loading_course: false,
+    search_teacher: null,
+    search_course: null,
+    select_teacher: null,
+    select_course: null,
+    teacher_name_box: [],
+    course_name_box: []
+  }),
+  mounted() {
+    axios.get(base_url + "/api/teacherList/all").then(response => {
+      this.teacher_name_box = response.data;
+    });
+    axios.get(base_url + "/api/courseList/all").then(response => {
+      this.course_name_box = response.data;
+    });
+  },
+  methods: {
+    getDataFac(fac) {
+      axios.get(base_url + "/api/faculty/" + fac).then(response => {
+        serverBus.$emit("serverSelected", response.data);
+      });
     },
-    methods: {
-        getDataFac(fac) {
-            axios.get(base_url + '/api/faculty/' + fac)
-                .then(response => {
-                    serverBus.$emit('serverSelected', response.data);
-                });
-        },
-        getDataDep(dep) {
-            axios.get(base_url + '/api/department/' + dep)
-                .then(response => {
-                    serverBus.$emit('serverSelected', response.data);
-                });
-        },
-        getDataTeacher(name) {
-            axios.get(base_url + '/api/teacher/' + name)
-                .then(response => {
-                    serverBus.$emit('serverSelected', response.data);
-                });
-        },
-        getDataCourse(cname) {
-            axios.get(base_url + '/api/course/' + cname)
-                .then(response => {
-                    serverBus.$emit('serverSelected', response.data);
-                });
-        },
-        querySelections_teacher(v) {
-            this.loading = true
-            // Simulated ajax query
-            setTimeout(() => {
-                this.items_teacher = this.teacher_name_box.filter(e => {
-                    return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
-                })
-                this.loading = false
-            }, 500)
-        },
-        querySelections_course(v) {
-            this.loading = true
-            // Simulated ajax query
-            setTimeout(() => {
-                this.items_course = this.course_name_box.filter(e => {
-                    return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
-                })
-                this.loading = false
-            }, 500)
-        }
+    getDataDep(dep) {
+      axios.get(base_url + "/api/department/" + dep).then(response => {
+        serverBus.$emit("serverSelected", response.data);
+        // console.log(response.data);
+      });
     },
-    watch: {
-        fac: function() {
-            axios.get(base_url + '/api/depList/' + this.fac)
-                .then(response => {
-                    this.department = response.data;
-                });
-        },
-        search_teacher(val) {
-            val && val !== this.select_teacher && this.querySelections_teacher(val);
-        },
-        search_course(val) {
-            val && val !== this.select_course && this.querySelections_course(val);
-        }
+    getDataTeacher(name) {
+      axios.get(base_url + "/api/teacher/" + name).then(response => {
+        serverBus.$emit("serverSelected", response.data);
+      });
+    },
+    getDataCourse(cname) {
+      axios.get(base_url + "/api/course/" + cname).then(response => {
+        serverBus.$emit("serverSelected", response.data);
+      });
+    },
+    querySelections_teacher(v) {
+      this.loading = true;
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items_teacher = this.teacher_name_box.filter(e => {
+          return (e || "").toLowerCase().indexOf((v || "").toLowerCase()) > -1;
+        });
+        this.loading = false;
+      }, 500);
+    },
+    querySelections_course(v) {
+      this.loading = true;
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items_course = this.course_name_box.filter(e => {
+          return (e || "").toLowerCase().indexOf((v || "").toLowerCase()) > -1;
+        });
+        this.loading = false;
+      }, 500);
     }
-}
+  },
+  watch: {
+    fac: function() {
+      axios.get(base_url + "/api/depList/" + this.fac).then(response => {
+        this.department = response.data;
+        // console.log("hook");
+      });
+    },
+    search_teacher(val) {
+      val && val !== this.select_teacher && this.querySelections_teacher(val);
+    },
+    search_course(val) {
+      val && val !== this.select_course && this.querySelections_course(val);
+    }
+  }
+};
 </script>
--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,74 +1,95 @@
 <template>
-    <v-card dark>
-        <v-card-title>
-            課程資訊
-            <v-spacer></v-spacer>
-            <v-text-field v-model="search" append-icon="search" label="篩選器 (任意輸入文字來篩選結果)" single-line hide-details></v-text-field>
-        </v-card-title>
-        <v-data-table :headers="headers" :items="info" :loading="isLoading" :search="search" hide-actions class="elevation-1" dark>
-            <v-progress-linear slot="progress" color="green" indeterminate></v-progress-linear>
-            <template slot="items" slot-scope="props">
-                <tr @click="showInfo(props.item.course_id)">
-                    <td class="text-xs">{{ props.item.course_cname }}</td>
-                    <td class="text-xs">{{ props.item.course_id }}</td>
-                    <td class="text-xs">{{ props.item.department }}</td>
-                    <td class="text-xs">{{ props.item.division }}</td>
-                    <td class="text-xs">{{ props.item.course_credit }}</td>
-                    <td class="text-xs">{{ props.item.time }}</td>
-                    <td class="text-xs">{{ props.item.location }}</td>
-                    <td class="text-xs">{{ props.item.teacher }}</td>
-                </tr>
-            </template>
-        </v-data-table>
-        <v-dialog v-model="dialog" max-width="40%">
-            <v-card >
-                <iframe style="width:100%; height: 700px" :src="dataUrl"></iframe>
-            </v-card>
-        </v-dialog>
-    </v-card>
+  <v-card dark>
+    <v-card-title>課程資訊
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="篩選器 (任意輸入文字來篩選結果)"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="info"
+      :loading="isLoading"
+      :search="search"
+      hide-actions
+      class="elevation-1"
+      dark
+    >
+      <v-progress-linear slot="progress" color="green" indeterminate></v-progress-linear>
+      <template slot="items" slot-scope="props">
+        <tr @click="showInfo(props.item.course_id)">
+          <td class="text-xs">{{ props.item.course_cname }}</td>
+          <td class="text-xs">{{ props.item.course_id }}</td>
+          <td class="text-xs">{{ props.item.department }}</td>
+          <td class="text-xs">{{ props.item.division }}</td>
+          <td class="text-xs">{{ props.item.course_credit }}</td>
+          <td class="text-xs">{{ props.item.time }}</td>
+          <td class="text-xs">{{ props.item.location }}</td>
+          <td class="text-xs">{{ props.item.teacher }}</td>
+        </tr>
+      </template>
+    </v-data-table>
+    <v-dialog v-model="dialog" max-width="40%">
+      <v-card>
+        <iframe style="width:100%; height: 700px" :src="dataUrl"></iframe>
+      </v-card>
+    </v-dialog>
+  </v-card>
 </template>
 <script>
-import axios from 'axios';
-import { serverBus, base_url } from '../main';
+import axios from "axios";
+import { serverBus, base_url } from "../main";
 export default {
-    data() {
-        return {
-            search: '',
-            headers: [{ text: '課程名稱', align: 'left', value: 'course_cname', sortable: false },
-                { text: '課號', value: 'course_id' },
-                { text: '開課單位', value: 'department' },
-                { text: '年級', value: 'division' },
-                { text: '學分', value: 'course_credit' },
-                { text: '時段', value: 'time' },
-                { text: '地點', value: 'location' },
-                { text: '教師姓名', value: 'teacher' }
-
-            ],
-            info: [],
-            isLoading: true,
-            dialog: false,
-            dataUrl: ''
-        }
-    },
-    created() {
-        // Using the service bus
-        this.isLoading = true;
-        serverBus.$on('serverSelected', (response) => {
-            this.info = response;
-        })
-    },
-    mounted() {
-        axios.get(base_url + '/api/20')
-            .then(response => {
-                this.info = response.data;
-                this.isLoading = false;
-            });
-    },
-    methods: {
-        showInfo(id) {
-            this.dataUrl = `https://ccweb.ncnu.edu.tw/student/current_semester_opened_listview.php?showdetail=&year=1071&courseid=${id}&class=0&modal=1&rnd=119581`;
-            this.dialog = true;
-        }
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "課程名稱",
+          align: "left",
+          value: "course_cname",
+          sortable: false
+        },
+        { text: "課號", value: "course_id" },
+        { text: "開課單位", value: "department" },
+        { text: "年級", value: "division" },
+        { text: "學分", value: "course_credit" },
+        { text: "時段", value: "time" },
+        { text: "地點", value: "location" },
+        { text: "教師姓名", value: "teacher" }
+      ],
+      info: [],
+      isLoading: true,
+      dialog: false,
+      dataUrl: ""
+    };
+  },
+  created() {
+    // Using the service bus
+    this.isLoading = true;
+    // let vm = this;
+    serverBus.$on("serverSelected", response => {
+      console.log("hook");
+      this.info = response;
+      // this.info = [];
+      // console.log(this.info);
+    });
+  },
+  mounted() {
+    axios.get(base_url + "/api/20").then(response => {
+      this.info = response.data;
+      this.isLoading = false;
+    });
+  },
+  methods: {
+    showInfo(id) {
+      this.dataUrl = `https://ccweb.ncnu.edu.tw/student/aspmaker_course_opened_detail_viewview.php?showdetail=&year=1072&courseid=${id}&class=0&modal=1&rnd=159132`;
+      this.dialog = true;
     }
-}
+  }
+};
 </script>
