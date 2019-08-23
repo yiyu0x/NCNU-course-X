6fa334e8bfbc1b1eaa76d4c500c398c3a03757d5
aFish <tako0903@icloud.com>
2019-08-23 16:59:16 +0800

    Import google analytics.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2016,
-        "sourceType": "module"
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        // ecmaVersion: 2016,
+        sourceType: 'module',
+        parser: 'babel-eslint',
+        ecmaVersion: 6
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always']
+    },
+    parser: 'babel-eslint'
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import './plugins/vuetify';
 import App from './App.vue';
+import VueAnalytics from 'vue-analytics';
 export const serverBus = new Vue();
 export const base_url = 'https://api.ncnusa.ml';
 Vue.config.productionTip = false;
 
-new Vue({
-    render: h => h(App),
-}).$mount('#app');
+Vue.use(VueAnalytics, {
+    id: 'UA-145409243-2'
+});
 
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
